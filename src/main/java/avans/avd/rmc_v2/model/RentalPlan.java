6a0fc16a46ec7b5856581ea422927805b34dcec2
BartWijnja <72634062+BartWijnja@users.noreply.github.com>
2021-12-06 20:18:53 +0100

    Revert "RMC-2 Added RentalPlan controller, service and Repo"

--- src/main/java/avans/avd/rmc_v2/model/RentalPlan.java
+++ src/main/java/avans/avd/rmc_v2/model/RentalPlan.java
@@ -1,37 +1,4 @@
 package avans.avd.rmc_v2.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class RentalPlan {
-    @Id
-    private Long id;
-    @ManyToOne
-    private Car car;
-    @ManyToOne
-    private User user;
-    private BigDecimal price;
-    private boolean inUse;
-    private LocalDate availableFrom;
-    private LocalDate availableUntil;
-    @CreationTimestamp
-    private LocalDateTime createdAt;
-    @UpdateTimestamp
-    private LocalDateTime updatedAt;
 }
--- src/main/java/avans/avd/rmc_v2/repository/CarRepository.java
+++ src/main/java/avans/avd/rmc_v2/repository/CarRepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
+
     Optional<Car> findCarByLicensePlateNumber(String licensePlateNumber);
 
 }
