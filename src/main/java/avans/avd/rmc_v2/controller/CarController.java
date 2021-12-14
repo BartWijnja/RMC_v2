5eedd43450d2861f8565cd2106345ade7bc4dc3f
BartWijnja <bart@wijnja.com>
2021-12-14 21:35:15 +0100

    adjusted naming endpoint

--- src/main/java/avans/avd/rmc_v2/controller/CarController.java
+++ src/main/java/avans/avd/rmc_v2/controller/CarController.java
@@ -45,7 +45,7 @@ public class CarController {
         return carService.deleteCar(id);
     }
 
-    @GetMapping("/tco/{id}/{kilometers}")
+    @GetMapping("/fuelcostperyear/{id}/{kilometers}")
     public double getFuelCostPerYear(@PathVariable Long id, @PathVariable double kilometers) {
         return carService.calculateFuelCostPerYear(id, kilometers);
     }
