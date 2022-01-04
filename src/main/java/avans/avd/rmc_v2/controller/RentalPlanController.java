43cd6e4c40cd810474e29c90a41eab8826afddbb
BartWijnja <bart@wijnja.com>
2022-01-04 21:04:40 +0100

    created getAllReservations endpoint

--- src/main/java/avans/avd/rmc_v2/controller/RentalPlanController.java
+++ src/main/java/avans/avd/rmc_v2/controller/RentalPlanController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path="api/v1/rentalplan/")
+@RequestMapping(path="api/v1/rentalplan")
 public class RentalPlanController {
     private final RentalPlanService rentalPlanService;
 
