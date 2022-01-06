704eabdb0d13809e198438e13a7d229eddc00c38
BartWijnja <bart@wijnja.com>
2022-01-06 20:12:25 +0100

    deleted timeslot model + controller/service/repository

--- src/main/java/avans/avd/rmc_v2/repository/TimeslotRepository.java
+++
@@ -1,9 +0,0 @@
-package avans.avd.rmc_v2.repository;
-
-import avans.avd.rmc_v2.model.Timeslot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TimeslotRepository extends JpaRepository<Timeslot, Long> {
-}
