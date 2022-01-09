35c40608c5392062ce9403c35e73d000d2317f7f
Ivan <i.ivanov.peev@hotmail.com>
2022-01-09 12:54:40 +0100

    added basic scheduling

--- src/main/java/com/rentmycar/RentmycarApplication.java
+++ src/main/java/com/rentmycar/RentmycarApplication.java
@@ -2,8 +2,10 @@ package com.rentmycar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RentmycarApplication {
 
 	public static void main(String[] args) {
